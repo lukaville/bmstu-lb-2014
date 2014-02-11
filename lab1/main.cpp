bcd647199566d3e202a3c8ceb41e9a49ec7a02a3
Nickolay Chameyev <lukaville@gmail.com>
2014-02-11 09:35:34 +0000

    Add array initialization

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 int main()
 {
-    /* Variable defenition */
+    /* Variable initialization */
     int int_var = 0;
     long long_var = 0;
     char char_var = 'a';
     float float_var = 0.1;
     double double_var = 0.1;
     bool bool_var = false;
+
+    /* Array initialization */
+    int int_arr[25] = {int_arr[22] = 1};
+    long long_arr[25] = {int_arr[22] = 1};
+    char char_arr[25] = {int_arr[22] = 'a'};
+    float float_arr[25] = {int_arr[22] = 0.1};
+    double double_arr[25] = {int_arr[22] = 0.1};
+    bool bool_arr[25] = {int_arr[22] = false};
 
     return 0;
 }
