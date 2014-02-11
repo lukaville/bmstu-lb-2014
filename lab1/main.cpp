4c6ae9abc523ab1baa2d436f65f93c7aa187b8ff
Nickolay Chameyev <lukaville@gmail.com>
2014-02-11 09:31:24 +0000

    Add variable defenitions

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    /* Variable defenition */
+    int int_var = 0;
+    long long_var = 0;
+    char char_var = 'a';
+    float float_var = 0.1;
+    double double_var = 0.1;
+    bool bool_var = false;
+
     return 0;
 }
-
