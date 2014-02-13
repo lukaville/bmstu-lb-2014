3264a4e2543b13a2e62bc0157fff23b22e745b13
Nickolay Chameyev <lukaville@gmail.com>
2014-02-13 10:27:22 +0000

    Input and output standart file types

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -9,8 +9,17 @@ int main()
     int int_var;
     cout << "Введите int_var: ";
     cin >> int_var;
-    cout << "Введено int_var = " << int_var << endl;
+    cout << "Введено int_var = " << int_var << endl << endl;
 
+    double double_var;
+    cout << "Введите double_var: ";
+    cin >> double_var;
+    cout << "Введено double_var = " << double_var << endl << endl;
+
+    char chars_var[100];
+    cout << "Введите chars_var: ";
+    cin >> chars_var;
+    cout << "Введено chars_var = " << chars_var << endl << endl;
 
 
     return 0;
