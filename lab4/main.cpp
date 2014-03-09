52fc79771d7444960fbb326b96cb64a0f9024073
Nickolay Chameyev <lukaville@gmail.com>
2014-03-09 14:46:16 +0000

    Using macros

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+#define max_of_three(a, b, c) (( a > b)?(( a > c)? a : c):((b >  c) ? b : c))
+
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // Using macros max of three
+    int v1 = 1;
+    double v2 = 1.1;
+    double v3 = 3.6;
+    const int v4 = 5.1;
+    const double v5 = 6.9;
+
+    cout << max_of_three(v1, v2, v3) << endl;
+    cout << max_of_three(v2, v3, v4) << endl;
+    cout << max_of_three(v3, v4, v5) << endl;
+
     return 0;
 }
 
