001749c15bcb8f575cecbfb31e3aaa0bd02adfec
Nickolay Chameyev <lukaville@gmail.com>
2014-03-10 21:15:15 +0000

    Default arguments with overload

--- lab5/lab5/main.cpp
+++ lab5/lab5/main.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 int main()
 {
+    // Default arguments in prototype
     cout << maximum(1, 2, 3) << endl;
     cout << maximum(1, 2) << endl;
     cout << maximum(1) << endl;
     cout << maximum() << endl;
+
+    // Default arguments with overload
+    cout << maximum1(1, 2, 3) << endl;
+    cout << maximum1(1, 2) << endl;
+    cout << maximum1(1) << endl;
+    cout << maximum1() << endl;
+
 
     return 0;
 }
--- lab5/lab5/main.h
+++ lab5/lab5/main.h
@@ -5,4 +5,27 @@ int maximum(int a = 0, int b = 0, int c = 0) {
     return ((a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c));
 }
 
+int maximum1(int a, int b, int c) {
+    return ((a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c));
+}
+
+int maximum1(int a, int b) {
+    int c = 0;
+    return ((a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c));
+}
+
+int maximum1(int a) {
+    int b = 0;
+    int c = 0;
+    return ((a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c));
+}
+
+int maximum1() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    return ((a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c));
+}
+
+
 #endif // MAIN_H
